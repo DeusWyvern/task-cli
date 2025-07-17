c3ff7b728c256ac4a4cf689cb481def89cf3c04f
DeusWyvern <dpefley98@gmail.com>
2025-07-17 16:37:24 -0700

    Implement Add command with hardcoded test.

--- src/task_cli/cli.py
+++ src/task_cli/cli.py
@@ -1,7 +1,10 @@
 import click
-from .list_commands import list
+import json
+import pathlib
+from . import list_commands
 
 
+# Entry point for the CLI
 @click.group()
 def cli():
     pass
@@ -11,7 +14,31 @@ def cli():
 @click.argument("name", type=click.STRING)
 def add(name):
     """Adds a task called <name> to the task list."""
-    click.echo(name)
+
+    current_path = pathlib.Path().resolve()
+    file_name = "test.json"
+    file_path = current_path.joinpath(file_name)
+
+    task_dictionary = {}
+    this_task_id = 1
+
+    if not file_path.is_file():
+        open(file_path, "w").close()
+
+    with open(file_path, "r") as openfile:
+        try:
+            task_dictionary = json.load(openfile)
+            task_id_list = list(task_dictionary.keys())
+            this_task_id = int(max(task_id_list)) + 1
+        except json.JSONDecodeError as e:
+            click.echo(f"{e}")
+
+    with open(file_path, "w") as openfile:
+        new_task = {this_task_id: name}
+        task_dictionary.update(new_task)
+        json.dump(task_dictionary, openfile)
+
+    click.echo(f"Added TASK <{name}> with ID <{this_task_id}> to file. Marked TODO.")
 
 
 @cli.command()
@@ -47,4 +74,4 @@ def md(id):
     click.echo(f"{id}")
 
 
-cli.add_command(list)
+cli.add_command(list_commands.list)
